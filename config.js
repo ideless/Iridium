b5eb9cd4664b87b3c2eb14e655b8a5e841fcf2f2
ubuntu <>
2022-05-08 12:05:54 +0800

    fix query_cur_region for chinese server

--- config.js
+++ config.js
@@ -1,39 +1,40 @@
 module.exports = {
-	ignoredProtos: [
-		// "QueryPathReq",
-		// "PingReq",
-		// "PingRsp",
-		// "UnionCmdNotify",
-		// "EvtAiSyncCombatThreatInfoNotify",
-		// "WorldPlayerRTTNotify",
-		// "QueryPathRsp",
-		// "EvtAiSyncSkillCdNotify",
-		// "SetEntityClientDataNotify",
-		// "ObstacleModifyNotify",
-		// "ClientReportNotify",
-		// "ClientAbilityInitFinishNotify",
-		// "EntityConfigHashNotify",
-		// "MonsterAIConfigHashNotify",
-		// "EntityAiSyncNotify",
-		"EvtAiSyncSkillCdNotify",
-		// //currently broken packets
+    ignoredProtos: [
+        "QueryPathReq",
+        "PingReq",
+        "PingRsp",
+        "UnionCmdNotify",
+        "EvtAiSyncCombatThreatInfoNotify",
+        "WorldPlayerRTTNotify",
+        "QueryPathRsp",
+        "EvtAiSyncSkillCdNotify",
+        "SetEntityClientDataNotify",
+        "ObstacleModifyNotify",
+        "ClientReportNotify",
+        "ClientAbilityInitFinishNotify",
+        "EntityConfigHashNotify",
+        "MonsterAIConfigHashNotify",
+        "EntityAiSyncNotify",
+        "EvtAiSyncSkillCdNotify",
+        // //currently broken packets
 
-		"TakeAchievementRewardRsp",
-		"ActivityPlayOpenAnimNotify",
-		"FurnitureCurModuleArrangeCountNotify",
-		"HomeAvatarTalkFinishInfoNotify",
-		"GroupLinkAllNotify",
-		"UnlockedFurnitureSuiteDataNotify",
-		"HomeAvatarRewardEventNotify",
-		"H5ActivityIdsNotify",
-		"HomePriorCheckNotify",
-		"HomePlantInfoNotify",
-		"HomeResourceNotify",
-		"HomeAvatarAllFinishRewardNotify",
-		"HomeBasicInfoNotify",
-		"FurnitureMakeRsp"
-	],
-	ProtosToDump:[
-	],  // add proto names here	to dump them
-	dumpAll : false, // dump all packets except ignored ones if set to true
+        "TakeAchievementRewardRsp",
+        "ActivityPlayOpenAnimNotify",
+        "FurnitureCurModuleArrangeCountNotify",
+        "HomeAvatarTalkFinishInfoNotify",
+        "GroupLinkAllNotify",
+        "UnlockedFurnitureSuiteDataNotify",
+        "HomeAvatarRewardEventNotify",
+        "H5ActivityIdsNotify",
+        "HomePriorCheckNotify",
+        "HomePlantInfoNotify",
+        "HomeResourceNotify",
+        "HomeAvatarAllFinishRewardNotify",
+        "HomeBasicInfoNotify",
+        "FurnitureMakeRsp"
+    ],
+    ProtosToDump: [
+        "PlayerStoreNotify"
+    ],  // add proto names here	to dump them
+    dumpAll: true, // dump all packets except ignored ones if set to true
 }
